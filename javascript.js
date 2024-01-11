abae8ef4aa60b585e9359cd258ec41c8b41f0c34
Vineeth <145092290+persistent-hustler@users.noreply.github.com>
2024-01-11 18:31:11 +0530

    Write a function to play a single round
    The function takes two parameters - the playerSelection and
    computerSelection - and then returns a string that declares the winner
    of the round.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,42 @@
 let computerChoice;
+getComputerChoice();
+let playerChoice = 'scissors';
+
+console.log(playRound(playerChoice, computerChoice));
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     switch(num) {
         case 0 :
-            computerChoice = 'Rock';
+            computerChoice = 'ROCK';
             break;
         case 1 :
-            computerChoice = 'Paper';
+            computerChoice = 'PAPER';
             break;
         case 2 :
-            computerChoice = 'Scissors';
+            computerChoice = 'SCISSORS';
             break;
         default:
             computerChoice = 'Computer couldn\'t choose';
     }
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    let message;
+    playerSelection = playerSelection.toUpperCase();
+    let loseMessage = `You Lose. ${computerSelection} beats ${playerSelection}.`;
+    let winMessage = `You Win. ${playerSelection} beats ${computerSelection}.`;
+    if (playerSelection === computerSelection) {
+        message = "That's a tie! Let's re-play that!";
+    } else if (playerSelection === 'ROCK') {
+        message = (computerSelection === 'PAPER') ? loseMessage : winMessage;
+    } else if (playerSelection === 'PAPER') {
+        message = (computerSelection === 'SCISSORS') ? loseMessage : winMessage;
+    } else if (playerSelection === 'SCISSORS') {
+        message = (computerSelection === 'ROCK') ? loseMessage : winMessage;
+    } else {
+        message = "You can only choose Rock, Paper or Scissors.";
+    }
+
+    return message;
+}
