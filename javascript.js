2dd6f604daf7aa5cde2f7fe7cf58cb772f0b2b80
Vineeth <145092290+persistent-hustler@users.noreply.github.com>
2024-01-30 19:46:31 +0530

    Make Paper and Scissors buttons functional

--- javascript.js
+++ javascript.js
@@ -1,3 +1,74 @@
+let playerWins = 0;
+let computerWins = 0;
+let computerChoice;
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const roundMessage = document.querySelector('#roundMessage');
+const finalMessage = document.querySelector('#finalMessage');
+const rock = document.querySelector('#ROCK');
+const paper = document.querySelector('#PAPER');
+const scissors = document.querySelector('#SCISSORS');
+
+
+rock.addEventListener('click', () => {
+    let roundResult = playRound('ROCK');
+    switch(roundResult) {
+        case 'win' :
+            playerWins++;
+            roundMessage.textContent = "Computer's choice: Scissors. You won the round.";
+            break;
+        case 'lose' :
+            computerWins++;
+            roundMessage.textContent = 'Computer chose Paper. You lost the round.';
+            break;
+        case 'tie' :
+            roundMessage.textContent = "We both went rock! That's a tie!";
+            break;
+    }
+    playerScore.textContent = playerWins;
+    computerScore.textContent = computerWins;
+});
+
+paper.addEventListener('click', () => {
+    let roundResult = playRound('PAPER');
+    switch(roundResult) {
+        case 'win' :
+            playerWins++;
+            roundMessage.textContent = "Computer's choice: Rock. You won the round.";
+            break;
+        case 'lose' :
+            computerWins++;
+            roundMessage.textContent = 'Computer chose Scissors. You lost the round.';
+            break;
+        case 'tie' :
+            roundMessage.textContent = "We both went Paper! That's a tie!";
+            break;
+    }
+    playerScore.textContent = playerWins;
+    computerScore.textContent = computerWins;
+});
+
+scissors.addEventListener('click', () => {
+    let roundResult = playRound('SCISSORS');
+    switch(roundResult) {
+        case 'win' :
+            playerWins++;
+            roundMessage.textContent = "Computer's choice: Paper. You won the round.";
+            break;
+        case 'lose' :
+            computerWins++;
+            roundMessage.textContent = 'Computer chose Rock. You lost the round.';
+            break;
+        case 'tie' :
+            roundMessage.textContent = "We both went Scissors! That's a tie!";
+            break;
+    }
+    playerScore.textContent = playerWins;
+    computerScore.textContent = computerWins;
+});
+
+// Function Declarations below
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     switch(num) {
@@ -15,48 +86,19 @@ function getComputerChoice() {
 function playRound(playerSelection) {
     let roundResult;
     playerSelection = playerSelection.toUpperCase();
-    computerSelection = getComputerChoice();
+    computerChoice = getComputerChoice();
 
-    if (playerSelection === computerSelection) {
+    if (playerSelection === computerChoice) {
         roundResult = 'tie';
     } else if (playerSelection === 'ROCK') {
-        roundResult = (computerSelection === 'PAPER') ? 'lose' : 'win';
+        roundResult = (computerChoice === 'PAPER') ? 'lose' : 'win';
     } else if (playerSelection === 'PAPER') {
-        roundResult = (computerSelection === 'SCISSORS') ? 'lose' : 'win';
+        roundResult = (computerChoice === 'SCISSORS') ? 'lose' : 'win';
     } else if (playerSelection === 'SCISSORS') {
-        roundResult = (computerSelection === 'ROCK') ? 'lose' : 'win';
+        roundResult = (computerChoice === 'ROCK') ? 'lose' : 'win';
     } else {
         roundResult = "Error";
     }
 
     return roundResult;
 }
-
-// Person has clicked on Rock. What now?
-
-let playerWins = 0;
-let computerWins = 0;
-const playerScore = document.querySelector('#playerScore');
-const computerScore = document.querySelector('#computerScore');
-const roundMessage = document.querySelector('#roundMessage');
-const finalMessage = document.querySelector('#finalMessage');
-
-const rock = document.querySelector('#ROCK');
-rock.addEventListener('click', () => {
-    let roundResult = playRound('ROCK');
-    switch(roundResult) {
-        case 'win' :
-            playerWins++;
-            roundMessage.textContent = "Computer's choice: scissors. You won the round.";
-            break;
-        case 'lose' :
-            computerWins++;
-            roundMessage.textContent = 'Computer chose Paper. You lost the round.';
-            break;
-        case 'tie' :
-            roundMessage.textContent = "We both went rock! That's a tie!";
-            break;
-    }
-    playerScore.textContent = playerWins;
-    computerScore.textContent = computerWins;
-});
